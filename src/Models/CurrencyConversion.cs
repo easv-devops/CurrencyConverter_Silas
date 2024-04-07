73d3d06bc7f3004f0cbf0aac52added9217c0861
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-04-07 09:23:58 +0200

    Update CurrencyConversion.cs

--- src/Models/CurrencyConversion.cs
+++ src/Models/CurrencyConversion.cs
@@ -6,13 +6,13 @@ namespace Models
         public string Source { get; set; }
         public string Target { get; set; }
         public int Value { get; set; }
-        public decimal Result { get; set; }
+        public double Result { get; set; }
 
         // Parameterless constructor
         public CurrencyConversion() { }
 
         // Constructor that matches the SQL query
-        public CurrencyConversion(DateTime date, string source, string target, int value, decimal result)
+        public CurrencyConversion(DateTime date, string source, string target, int value, double result)
         {
             Date = date;
             Source = source;
