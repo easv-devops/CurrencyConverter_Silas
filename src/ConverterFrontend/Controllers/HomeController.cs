e562eee155626afa8d7324ff55ce68340cdbee76
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-04-09 18:16:25 +0200

    Update HomeController.cs

--- src/ConverterFrontend/Controllers/HomeController.cs
+++ src/ConverterFrontend/Controllers/HomeController.cs
@@ -22,12 +22,6 @@ public class HomeController : Controller
         _httpClient = new HttpClient();
         _featureHubConfig = new EdgeFeatureHubConfig("http://featurehub:8085/", "7aafd1c5-0ca6-4086-83f4-ecc8c8f44d1e/1BAw3HQvXeLd5O5dhTqaG4lCfmP1ooNgKiTNS71L");
         SetBaseAddress("http://converter-api:8080/");
-        
-        Task.Run(async () =>
-        {
-            var fh = await _featureHubConfig.NewContext().Build();
-            historyEnabled = fh["history"].IsEnabled;
-        });
     }
     
     public void SetBaseAddress(string baseAddress)
@@ -37,6 +31,9 @@ public class HomeController : Controller
 
     public async Task<IActionResult> Index()
     {
+        var fh = await _featureHubConfig.NewContext().Build();
+        historyEnabled = fh["history"].IsEnabled;
+        
         var result = await _httpClient.GetAsync(_httpClient.BaseAddress + "currencyconverter");
         var conversions = await result.Content.ReadFromJsonAsync<CurrencyConversion[]>();
         var model = new IndexViewModel
@@ -49,12 +46,12 @@ public class HomeController : Controller
         return View(model);
     }
 
-    public async Task<IActionResult> _History()
+    /*public async Task<IActionResult> _History()
     {
         var result = await _httpClient.GetAsync(_httpClient.BaseAddress + "currencyconverter");
         var conversions = await result.Content.ReadFromJsonAsync<CurrencyConversion[]>();
         return View(conversions);
-    }
+    }*/
 
     [HttpPost]
     public async Task<IActionResult> ConvertCurrency(string source, string target, int value)
